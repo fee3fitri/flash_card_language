5f94fc6269f40ddcee03f777839303087ccb5e8f
fee3fitri <110148438+fee3fitri@users.noreply.github.com>
2024-02-29 15:32:45 -0800

    Convert the buttons from Canvas to Button class

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ BG_COLOR = "#B1DDC6"
 
 # Create Tkinter window
 window = Tk()
-window.title("French - English Flash Card")
+window.title("Flash Card")
 window.config(padx=50, pady=50, background=BG_COLOR)
 
 # Card Components
@@ -14,24 +14,17 @@ card_front = Canvas(width=800, height=526, highlightthickness=0, bg=BG_COLOR)
 card_front_img = PhotoImage(file="images/card_front.png")
 card_front.create_image(400, 263, image=card_front_img)
 card_front.grid(row=0, column=0, rowspan=2, columnspan=2)
+card_front.create_text(400, 175, text="French", font=("Ariel", 30, "italic"))
+card_front.create_text(400, 275, text="trouve", font=("Ariel", 50, "bold"))
 
-# Front Card Label
-french_label = Label(text="French", bg="white", font=("Ariel", 30, "italic"))
-french_label.grid(row=0, column=0, columnspan=2)
-french_label.config(pady=50)
-
-french_word_label = Label(text="trouve", bg="white", font=("Ariel", 50, "bold"))
-french_word_label.grid(row=0, column=0, rowspan=3, columnspan=2)
 
 # Yes No Button Images
-no_btn = Canvas(width=100, height=100, highlightthickness=0, bg=BG_COLOR)
-no_img = PhotoImage(file="images/wrong.png")
-no_btn.create_image(50, 50, image=no_img)
-no_btn.grid(row=3, column=0)
+cross_img = PhotoImage(file="images/wrong.png")
+cross_btn = Button(image=cross_img, highlightthickness=0, cursor="hand2")
+cross_btn.grid(row=3, column=0)
 
-yes_btn = Canvas(width=100, height=100, highlightthickness=0, bg=BG_COLOR)
-yes_img = PhotoImage(file="images/right.png")
-yes_btn.create_image(50, 50, image=yes_img)
-yes_btn.grid(row=3, column=1)
+check_img = PhotoImage(file="images/right.png")
+check_btn = Button(image=check_img, highlightthickness=0, cursor="hand2")
+check_btn.grid(row=3, column=1)
 
 window.mainloop()
